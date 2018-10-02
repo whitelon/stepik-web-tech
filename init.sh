6fefcb11d4b725601fb75d417719a82ecbe413ef
whitelon <happysensey@gmail.com>
2018-10-03 02:35:40 +0300

    some magic

--- init.sh
+++ init.sh
@@ -1,2 +1,4 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+echo on
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
+sudo unlink /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
