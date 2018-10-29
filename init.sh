a9fc7a06acc59198dccd73876b7aff659ef3bd99
whitelon <happysensey@gmail.com>
2018-10-29 17:21:10 +0300

    fix link gunicorn conf

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 echo on
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
 sudo unlink /etc/nginx/sites-enabled/default
-sudo ln -s etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/nginx restart
 gunicorn -c /etc/gunicorn.d/hello.py hello:wsgi_application
