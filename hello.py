e54f54b2f2e73d2067e2a938f6e73973d70225a1
whitelon <happysensey@gmail.com>
2018-10-29 17:16:40 +0300

    wsgi task

---
+++ hello.py
@@ -0,0 +1,9 @@
+def wsgi_application(environ, start_response):
+    
+    body = environ['QUERY_STRING'].replace("&", "\n")
+    status = '200 OK'
+    headers = [
+        ('Content-Type', 'text/plain')
+    ]
+    start_response(status, headers)
+    return [body]
