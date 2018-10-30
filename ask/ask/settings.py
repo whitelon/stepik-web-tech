3e8082d1cf640d97b05914fff66e1c28e0874f2a
whitelon <happysensey@gmail.com>
2018-10-30 16:36:54 +0300

    django settings

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'sa$!nee!m^t0pw1tm$-#ip_-nj1ry8ry9&(38hq(q5aat8k=7)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ask',
+    'qa'
 ]
 
 MIDDLEWARE = [
